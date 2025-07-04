9d16b5be6e6f646b95597ec6d91cdfbfe324d96f
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-07-04 14:36:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 
+// import React from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//     </div>
+//   )
+// }
+
+// export default App
+
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Hello, welcome to the React App!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
